e1cb42d0206d16ee86e7f3810ecba3ddc55dc516
Jose Luis Bendezu Cortez <bendezucortezjoseluis@gmail.com>
2022-09-28 07:04:47 -0500

    Validacion de Formulario y Mostrar Mensaje Error

--- js/app.js
+++ js/app.js
@@ -8,11 +8,21 @@ const fechaInput = document.querySelector('#fecha');
 const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
 
+//UI
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
 
 eventListener();
 
 function eventListener() {
-    mascotaInput.addEventListener('input',datosCita)
+    mascotaInput.addEventListener('input',datosCita);
+    propietarioInput.addEventListener('input',datosCita);
+    telefonoInput.addEventListener('input',datosCita);
+    fechaInput.addEventListener('input',datosCita);
+    horaInput.addEventListener('input',datosCita);
+    sintomasInput.addEventListener('input',datosCita);
+
+    formulario.addEventListener('submit',nuevaCita);
 }
 
 //Objeto Cita
@@ -26,11 +36,61 @@ const ObjCita = {
     sintomas: ''
 }
 
+//Clases
+class UI {
+
+    imprimirAlerta(mensaje,tipo)
+    {
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center','alert','d-block','col-12');
+
+        if(tipo === 'error')
+        {
+            divMensaje.classList.add('alert-danger');
+        }else{
+            divMensaje.classList.add('alert-success');
+        }
+
+        divMensaje.textContent = mensaje;
+
+        const contenido = document.querySelector('#contenido');
+        contenido.insertBefore(divMensaje,document.querySelector('.agregar-cita'));
+    
+    setTimeout(() => {
+        divMensaje.remove();
+    }, 3000);
+    }
+}
+
+class Citas {
+    constructor()
+    {
+        this.citas = [];
+    }
+}
+
+//Instanciamos
+
+const ui = new UI();
+const administrarCitas = new Citas();
+
 //Funciones
 
 function datosCita(e){
     ObjCita[e.target.name] = e.target.value;
-    console.log(ObjCita);
+    
+}
+
+function nuevaCita(e) {
+    e.preventDefault();
+    const {mascota,propietario,telefono,fecha,hora,sintomas} = ObjCita;
+
+    if(mascota === '' || propietario === '' || telefono === '' || fecha === '' || hora === '' || sintomas === '')
+    {
+        ui.imprimirAlerta('Todos los campos son Obligatorios','error');
+
+        return;
+    }
 }
 
 
